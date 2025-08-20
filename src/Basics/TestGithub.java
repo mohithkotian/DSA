4136d438c28919bc60d8310883b6c78e26e302f2
mohithkotian <kotianmohith907@gmail.com>
2025-08-21 01:27:52 +0530

    Testing....

---
+++ src/Basics/TestGithub.java
@@ -0,0 +1,7 @@
+package Basics;
+
+public class TestGithub {
+    public static void main(String[] args) {
+        System.out.println("Just testing github if files are getting uploaded or not fuck u if are reading thisgit ");
+    }
+}
