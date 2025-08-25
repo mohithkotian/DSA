29ac776a8dc1509c818f44c09667e34c41c25064
mohithkotian <kotianmohith907@gmail.com>
2025-08-25 23:02:13 +0530

    yeah

---
+++ src/Basics/Arrays/Multidimenstion.java
@@ -0,0 +1,15 @@
+package Basics.Arrays;
+
+public class Multidimenstion {
+    public static void main(String[] args) {
+        /*
+        123
+        456
+        789
+         */
+    }//stored like array in the  array
+    int[][] arr ={{1,2,3},{4,5,6},{7,8,9}
+
+    };
+
+}
