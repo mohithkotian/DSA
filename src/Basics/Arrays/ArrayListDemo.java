9212832d9a3530523df29c3eb6cb39c8adbb144d
mohithkotian <kotianmohith907@gmail.com>
2025-08-27 21:43:34 +0530

    After got grind begins

---
+++ src/Basics/Arrays/ArrayListDemo.java
@@ -0,0 +1,15 @@
+package Basics.Arrays;
+
+import java.util.ArrayList;
+public class ArrayListDemo {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>(10);
+        list.add(5);
+        list.add(20);
+        //can be added  any number input but here size is fixed
+        //why this in time complexity  0(1)
+        //working  basically it doubles the input or increasing the capacity
+        //
+        System.out.println(list);
+    }
+}
---
+++ src/Basics/Arrays/Swaap.java
@@ -0,0 +1,15 @@
+package Basics.Arrays;
+import java.util.Arrays;
+public class Swaap {
+    public static void main(String[] args) {
+        int[] arr= {1 , 2 ,5 ,4,45};
+        swap(arr,1,3);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void swap(int[] arr,int index1,int index2){
+        int temp =arr[index1];
+        arr[index1]=arr[index2];
+        arr[index2]=temp;
+
+    }
+}
