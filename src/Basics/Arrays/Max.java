06803bd6637b5bed9fddc4dc8de1910387e26e7d
mohithkotian <kotianmohith907@gmail.com>
2025-08-30 22:10:18 +0530

    Just a revision on Output, Input, Variables, Operators

--- src/Basics/Arrays/Max.java
+++ src/Basics/Arrays/Max.java
@@ -1,14 +1,19 @@
 package Basics.Arrays;
 import java.util.Arrays;
+
 public class Max {
     public static void main(String[] args) {
-        int[] arr= {1 , 2 ,5 ,4,45};
+        int[] arr = {1, 2, 5, 4, 45};
         System.out.println(max(arr));
     }
-    static int max(int[]arr){
-        int Maxval=arr[0];
-        for (int i = 0; i <arr.length ; i++) {
-            
+
+    static int max(int[] arr) {
+        int Maxval=arr[0]; // first element is max
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i]>Maxval) {
+                Maxval=arr[i];
+            }
         }
+        return Maxval;
     }
 }
