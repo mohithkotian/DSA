86b87b444a1d1adb008b96cd66938ca31ff79cda
mohithkotian <kotianmohith907@gmail.com>
2025-08-31 16:21:30 +0530

    Back to some basics Variables & Input/Output in Java

--- src/Basics/Arrays/ArrayListDemo.java
+++ src/Basics/Arrays/ArrayListDemo.java
@@ -6,13 +6,11 @@ public class ArrayListDemo {
         ArrayList<Integer> list = new ArrayList<>(10);
         list.add(5);
         list.add(20);
-<<<<<<< HEAD
         //can be added  any number input but here size is fixed
         //why this in time complexity  0(1)
         //working  basically it doubles the input or increasing the capacity
         //
-=======
->>>>>>> 1c6cd90a8c1572e60e6c61eaf983de82df1ff9ee
+
         System.out.println(list);
     }
 }
